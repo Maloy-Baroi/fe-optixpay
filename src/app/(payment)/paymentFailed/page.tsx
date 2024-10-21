f5db788091e16f2000f543306f4a8dc2c01de39a
maloy-baroi <maloy.virat@gmail.com>
2024-10-21 22:58:44 +0600

    updated failed payment page

--- src/app/(payment)/paymentFailed/page.tsx
+++ src/app/(payment)/paymentFailed/page.tsx
@@ -1,6 +1,21 @@
 "use client";
 
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 const PaymentFailed = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Use a function inside setTimeout
+    const timeout = setTimeout(() => {
+      router.push("/"); // Redirect to the homepage after 3 seconds
+    }, 3000);
+
+    // Clear the timeout when the component unmounts
+    return () => clearTimeout(timeout);
+  }, [router]);
+
   return (
     <div className="flex items-center justify-center h-screen">
       <h1>Payment Failed. Please try again.</h1>
